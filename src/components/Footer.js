1c10c32eb5407428376c45af6a66482169c56734
ckdye1 <chris96kl@gmail.com>
2024-01-03 22:13:42 +0000

    style updates (#6)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,123 +1,68 @@
+import '../styles/Footer.css';
 import React from "react";
+import logo from '../images/logo.png';
+import Nav from "react-bootstrap/Nav";
 
 const Footer = () => {
-
     return (
-        <section
-            className="elementor-section elementor-element elementor-element-2f2d3df2 elementor-section-boxed"
-            data-id="2f2d3df2" data-element_type="section"
-            data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+        <section className="elementor-section elementor-element elementor-element-2f2d3df2 elementor-section-boxed">
             <div className="elementor-background-overlay"></div>
             <div className="elementor-container elementor-column-gap-default">
                 <div
-                    className="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-36712176"
-                    data-id="36712176" data-element_type="column">
+                    className="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-36712176">
                     <div className="elementor-widget-wrap elementor-element-populated">
-                        <img
-                            src="./images/logo.png"
-                            className="attachment-large size-large wp-image-10" alt="" loading="lazy"
+                        <img src={logo}
+                             className="attachment-large size-large wp-image-10" alt="George Automotive" loading="lazy"
                         />
                         <div
-                            className="elementor-element elementor-element-4fd2e109 elementor-widget elementor-widget-text-editor"
-                            data-id="4fd2e109" data-element_type="widget"
-                            data-widget_type="text-editor.default">
+                            className="elementor-element elementor-element-4fd2e109 elementor-widget elementor-widget-text-editor">
                             <div className="elementor-widget-container">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
-                                    luctus nec
-                                    ullamcorper mattis, pulvinar dapibus leo.</p></div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec
+                                    ullamcorper mattis, pulvinar dapibus leo.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div
-                    className="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-418892f0"
-                    data-id="418892f0" data-element_type="column">
+                    className="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-418892f0">
                     <div className="elementor-widget-wrap elementor-element-populated">
-                        <div
-                            className="elementor-element elementor-element-45c474ce elementor-widget"
-                            data-id="45c474ce" data-element_type="widget" data-widget_type="heading.default">
+                        <div className="elementor-element elementor-element-45c474ce elementor-widget">
                             <div className="elementor-widget-container">
                                 <h4 className="elementor-heading-title">Company.</h4>
                             </div>
                         </div>
                         <div className="divider"></div>
                         <div
-                            className="elementor-element elementor-element-5c0eb2ed elementor-nav-menu__align-left elementor-widget elementor-widget-nav-menu"
-                            data-id="5c0eb2ed" data-element_type="widget"
-                            data-settings="{&quot;layout&quot;:&quot;vertical&quot;,&quot;submenu_icon&quot;:{&quot;value&quot;:&quot;&lt;i class=\&quot;fas fa-caret-down\&quot;&gt;&lt;\/i&gt;&quot;,&quot;library&quot;:&quot;fa-solid&quot;}}"
-                            data-widget_type="nav-menu.default">
+                            className="elementor-element elementor-element-5c0eb2ed elementor-widget elementor-widget-nav-menu">
                             <div className="elementor-widget-container">
-                                <nav
-                                    className="elementor-nav-menu--main elementor-nav-menu__container elementor-nav-menu--layout-vertical e--pointer-none">
-                                    <ul id="menu-1-5c0eb2ed" className="elementor-nav-menu sm-vertical">
-                                        <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-44">
-                                            <a href="#"
-                                               className="elementor-item">Home</a>
-                                        </li>
-                                        <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-46">
-                                            <a href="#"
-                                               className="elementor-item">About Us</a>
-                                        </li>
-                                        <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-47">
-                                            <a href="#"
-                                               className="elementor-item">Service</a>
-                                        </li>
-                                        <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-48">
-                                            <a href="#"
-                                               className="elementor-item">Contact Us</a>
-                                        </li>
-                                        <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-49">
-                                            <a href="#"
-                                               className="elementor-item elementor-item-anchor">Pages</a>
-                                        </li>
-                                    </ul>
-                                </nav>
+                                <Nav defaultActiveKey="/home" className="flex-column">
+                                    <Nav.Link href="#home">Home</Nav.Link>
+                                    <Nav.Link href="#about-us">About Us</Nav.Link>
+                                    <Nav.Link href="#services">Services</Nav.Link>
+                                    <Nav.Link href="#reviews">Reviews</Nav.Link>
+                                    <Nav.Link href="#contact-us">Contact Us</Nav.Link>
+                                </Nav>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div
-                    className="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-3b1d8fe1"
-                    data-id="3b1d8fe1" data-element_type="column">
+                    className="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-3b1d8fe1">
                     <div className="elementor-widget-wrap elementor-element-populated">
-                        <div
-                            className="elementor-element elementor-element-133be5c elementor-widget"
-                            data-id="133be5c" data-element_type="widget" data-widget_type="heading.default">
+                        <div className="elementor-element elementor-element-133be5c elementor-widget">
                             <div className="elementor-widget-container">
-                                <h4 className="elementor-heading-title">Useful Link</h4>
+                                <h4 className="elementor-heading-title">Socials.</h4>
                             </div>
                         </div>
                         <div className="divider"></div>
                         <div
-                            className="elementor-element elementor-element-76a53625 elementor-nav-menu__align-left elementor-widget elementor-widget-nav-menu"
-                            data-id="76a53625" data-element_type="widget"
-                            data-settings="{&quot;layout&quot;:&quot;vertical&quot;,&quot;submenu_icon&quot;:{&quot;value&quot;:&quot;&lt;i class=\&quot;fas fa-caret-down\&quot;&gt;&lt;\/i&gt;&quot;,&quot;library&quot;:&quot;fa-solid&quot;}}"
-                            data-widget_type="nav-menu.default">
+                            className="elementor-element elementor-element-76a53625 elementor-widget elementor-widget-nav-menu">
                             <div className="elementor-widget-container">
-                                <nav
-                                    className="elementor-nav-menu--main elementor-nav-menu__container elementor-nav-menu--layout-vertical e--pointer-none">
-                                    <ul id="menu-1-76a53625" className="elementor-nav-menu sm-vertical">
-                                        <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-44">
-                                            <a href="#"
-                                               className="elementor-item">Home</a>
-                                        </li>
-                                        <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-46">
-                                            <a href="#"
-                                               className="elementor-item">About Us</a>
-                                        </li>
-                                        <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-47">
-                                            <a href="#"
-                                               className="elementor-item">Service</a>
-                                        </li>
-                                        <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-48">
-                                            <a href="#"
-                                               className="elementor-item">Contact Us</a>
-                                        </li>
-                                        <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-49">
-                                            <a href="#"
-                                               className="elementor-item elementor-item-anchor">Pages</a>
-                                        </li>
-                                    </ul>
-                                </nav>
+                                <Nav defaultActiveKey="/home" className="flex-column">
+                                    <Nav.Link href="#home">Instagram</Nav.Link>
+                                    <Nav.Link href="#about-us">Facebook</Nav.Link>
+                                    <Nav.Link href="#services">Google Business</Nav.Link>
+                                </Nav>
                             </div>
                         </div>
                     </div>
